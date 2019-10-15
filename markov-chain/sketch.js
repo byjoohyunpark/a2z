26913c9b29d7675389684057472c9f925e019cb1
Joohyun Park <jhp527@nyu.edu>
2019-10-14 20:13:29 -0400

    refactor structure

--- markov-chain/sketch.js
+++ markov-chain/sketch.js
@@ -19,36 +19,23 @@ function setup() {
     // load text into the model
     markov.loadText(data.join(' '));
 
-
     for (let i = 0; i < (data.length + 1) / 2; i++) {
         sentence = data[i * 2];
         wrapper[i] = sentence;
 
         initial = createP(wrapper[i]);
+        initial.parent('content')
 
         val[i] = RiTa.splitSentences(wrapper[i]);
     }
 
 }
 
-
-
-function mouseClicked() {
-    reorganize();
-}
-
 function reorganize() {
-    document.body.innerHTML = '<h2>Variation on Barack Obama’s 2009 Nobel Peace Prize Speech</h2>'
+    document.querySelector('#content').innerHTML = ''
     for (let i = 0; i < wrapper.length; i++) {
         sentences = markov.generateSentences(val[i].length);
         let final = join(sentences, ' ');
-        createP(final);
+        createP(final).parent('content');
     }
 }
-
-$(function () {
-    $(window).bind("tap", tapHandler);
-    function tapHandler(event) {
-        reorganize();
-    }
-});
